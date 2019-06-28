3bafe5a5ae5cb451297369a60ea46d98182ea940
Tyler Ferrin <tyferrin@gmail.com>
2019-06-28 16:12:32 -0700

    indentation updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,33 @@
 <template>
   <section class="index">
-		<transition
-		appear
-		name="index-fade"
-		mode="out-in">
-			<ProjectGrid
-				v-if="isProjects"
-				:projects="projects"	/>
-			<About
-				v-if="!isProjects"
-				:description="description"	/>
-		</transition>
-		<!-- <SideNav /> -->
+    <transition appear name="index-fade" mode="out-in">
+      <ProjectGrid v-if="isProjects" :projects="projects"/>
+      <About v-if="!isProjects" :description="description"/>
+    </transition>
+    <!-- <SideNav /> -->
   </section>
 </template>
 
 <script>
-import ProjectGrid from '../components/ProjectGrid'
+import ProjectGrid from "../components/ProjectGrid";
 // import SideNav from '../components/SideNav'
-import About from '../components/About'
-import { mapState } from 'vuex'
+import About from "../components/About";
+import { mapState } from "vuex";
 
 export default {
-	components: {
-		// SideNav,
-		ProjectGrid,
-		About
-	},
-	data () {
-		return {
-			projects: this.$store.state.projects,
-			description: this.$store.state.description
-		}
-	},
-	computed: mapState([
-		'isProjects'
-	])
-}
+  components: {
+    // SideNav,
+    ProjectGrid,
+    About
+  },
+  data() {
+    return {
+      projects: this.$store.state.projects,
+      description: this.$store.state.description
+    };
+  },
+  computed: mapState(["isProjects"])
+};
 </script>
 
 <style lang="sass">
@@ -47,8 +38,6 @@ export default {
 		grid-template-columns: repeat(8, minmax(auto, calc(100vw/8)))
 		width: 100%
 
-
-	// index component transition classes
 	.index-fade-enter-active,
 	.index-fade-leave-active
 		transition: opacity .2s cubic-bezier(0.55, 0.055, 0.675, 0.19)
@@ -58,6 +47,4 @@ export default {
 
 	.index-fade-enter
 		opacity: 0
-
-
 </style>
